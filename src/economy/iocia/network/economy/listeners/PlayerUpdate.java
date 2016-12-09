ef72bda6ca320f0b327b2c1c8c9b6e2a999e5810
Mark Lisoway <lisowaym@gmail.com>
2016-12-09 03:32:42 -0700

    Combined join and quit events
    Combined the PlayerJoin and PlayerLeave event classes into a single
    class.

--- src/economy/iocia/network/economy/listeners/PlayerUpdate.java
+++ src/economy/iocia/network/economy/listeners/PlayerUpdate.java
@@ -4,18 +4,23 @@ import iocia.network.economy.currency.PlayerCoinController;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
 
-
-/**
- * Used to load a player's profile upon joining the server.
- */
-public class PlayerJoin implements Listener {
+public class PlayerUpdate implements Listener {
 
     @EventHandler
     public void onJoin(PlayerJoinEvent e) {
 
         PlayerCoinController.initializePlayerProfile(e.getPlayer());
         PlayerCoinController.loadPlayerProfile(e.getPlayer());
+
+    }
+
+    @EventHandler
+    public void onLeave(PlayerQuitEvent e) {
+
+        PlayerCoinController.savePlayerProfile(e.getPlayer());
+        PlayerCoinController.removePlayer(e.getPlayer());
 
     }
 
